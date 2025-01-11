e5d0ece0c668b6179e6974f585dc29beebb13d6f
Heitor Ramos <heitorvieira1220@gmail.com>
2025-01-10 23:00:40 -0300

    adição permissão para o swagger

--- apiProjeto/src/main/java/com/br/emakers/apiProjeto/infra/security/SecurityConfigurations.java
+++ apiProjeto/src/main/java/com/br/emakers/apiProjeto/infra/security/SecurityConfigurations.java
@@ -27,6 +27,7 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST).hasRole("ADMIN")
